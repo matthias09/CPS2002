aadc51bc8409f6164e1d916b05bde0d85467faaf
Matthias <matthias.attard.9@gmail.com>
2020-05-02 14:59:11 +0200

    Created move method in class

--- src/main/java/um/cps2002/Player.java
+++ src/main/java/um/cps2002/Player.java
@@ -14,7 +14,37 @@ public class Player {
         this.playerNum = num;
     }
 
-    public Player() {
+    public Position move(Map map) {
+        char direction = 0;
+        boolean valid = false;
 
+        while(!valid) {
+            direction = sc.next().charAt(0);
+
+            if((direction == 'U' && this.position.y == 0)
+                    || (direction == 'D' && this.position.y == map.size - 1)
+                    || (direction == 'L' && this.position.x == 0)
+                    || ((direction == 'R' && this.position.x == map.size - 1))){
+                System.out.println("Invalid move please try again");
+            }else if (direction == 'U' || direction == 'D' || direction == 'L' || direction == 'R')
+                valid = true;
+        }
+        switch (direction) {
+            case 'U':
+                this.position.y--;
+                break;
+            case 'D':
+                this.position.y++;
+                break;
+            case 'L':
+                this.position.x--;
+                break;
+            case 'R':
+                this.position.x++;
+                break;
+            default:
+                System.out.println("Error");
+        }
+        return this.position;
     }
 }
