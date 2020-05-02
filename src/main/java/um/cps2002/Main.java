0e46eb2952bdb75f1504fc79a6b3286c99860915
Matthias <matthias.attard.9@gmail.com>
2020-05-02 15:12:26 +0200

    Added get map size method

--- src/main/java/um/cps2002/Main.java
+++ src/main/java/um/cps2002/Main.java
@@ -26,6 +26,32 @@ public class Main {
         }
         return n_players;
     }
+
+    private int get_map_size(int n_players){
+        int map_size;
+
+        //Validating map size input
+        while (true) {
+            try{
+                if (n_players < 4) {
+                    System.out.println("Please input the size of the square map (Min = 5, Max = 50):");
+                } else {
+                    System.out.println("Please input the size of the square map (Min = 8, Max = 50):");
+                }
+                map_size = sc.nextInt();
+            }catch (Exception e){
+                System.out.println("Incorrect input");
+                continue;
+            }
+
+            if ((map_size < 5 && n_players <= 4) || (map_size < 8 && n_players > 4) || map_size > 50) {
+                System.out.println("Input is not in acceptable range.");
+            } else {
+                break;
+            }
+        }
+        return map_size;
+    }
     public static void main(String[] args) {
 
     }
