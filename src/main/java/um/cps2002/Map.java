1ee573593f5752a4ba975aaa0acd70adbcf06ba5
Matthias <matthias.attard.9@gmail.com>
2020-05-02 14:35:05 +0200

    Created map class and its variable and constructor

--- src/main/java/um/cps2002/Map.java
+++ src/main/java/um/cps2002/Map.java
@@ -1,4 +1,15 @@
 package um.cps2002;
+import java.util.Random;
 
 public class Map {
+    private Random rand = new Random();
+
+    //Variables
+    int size;
+    private String [][] map;
+
+    public Map(int size){
+        this.size = size;
+        map = new String[size][size];
+    }
 }
