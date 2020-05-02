52fff564bc004dfaee18b97bbff4ab823763f84d
Matthias <matthias.attard.9@gmail.com>
2020-05-02 15:11:46 +0200

    Added get number of players method

--- src/main/java/um/cps2002/Main.java
+++ src/main/java/um/cps2002/Main.java
@@ -1,7 +1,31 @@
 package um.cps2002;
 
 
+import java.util.Scanner;
+
 public class Main {
+    private Scanner sc = new Scanner(System.in);
+
+    private int get_num_players(){
+        //Validating number of players input
+        int n_players;
+
+        while (true) {
+            try {
+                System.out.println("Please input the number of players (Min = 2, Max = 8):");
+                n_players = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Incorrect input");
+                continue;
+            }
+            if (n_players < 2 || n_players > 8) {
+                System.out.println("Input is not in acceptable range.");
+            } else {
+                break;
+            }
+        }
+        return n_players;
+    }
     public static void main(String[] args) {
 
     }
