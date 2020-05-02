e94382f0ae2adf7b2e73688cdb8195b8f59baf58
Daniel Sumler <d.sumler1@outlook.com>
2020-05-02 15:48:56 +0200

    Added HTML Component

--- src/main/java/um/cps2002/Player.java
+++ src/main/java/um/cps2002/Player.java
@@ -1,20 +1,20 @@
-package um.cps2002;
+package com.company;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Player {
+class Player {
     private Random rand = new Random();
     private Scanner sc = new Scanner(System.in);
 
-    public Position position = new Position();
-    public Position start = new Position();
+    Position position = new Position();
+    Position start = new Position();
     private int playerNum;
 
-    public Player(int num) {
+    Player(int num){
         this.playerNum = num;
     }
 
-    public Position move(Map map) {
+    Position move(Map map) {
         char direction = 0;
         boolean valid = false;
 
@@ -22,9 +22,9 @@ public class Player {
             direction = sc.next().charAt(0);
 
             if((direction == 'U' && this.position.y == 0)
-                    || (direction == 'D' && this.position.y == map.size - 1)
-                    || (direction == 'L' && this.position.x == 0)
-                    || ((direction == 'R' && this.position.x == map.size - 1))){
+                || (direction == 'D' && this.position.y == map.size - 1)
+                || (direction == 'L' && this.position.x == 0)
+                || ((direction == 'R' && this.position.x == map.size - 1))){
                 System.out.println("Invalid move please try again");
             }else if (direction == 'U' || direction == 'D' || direction == 'L' || direction == 'R')
                 valid = true;
@@ -80,4 +80,7 @@ public class Player {
     Position getStart(){
         return this.start;
     }
+
 }
+
+
